3c1185300602cd3fc6a1940a653d7554491daf6c
kwakkun2002 <36096367+kwakkun2002@users.noreply.github.com>
2019-11-07 11:30:40 +0900

    11/07 어느정도 완성됨
    가자고

--- app/src/main/java/com/example/booktobook/SearchFragment.java
+++ app/src/main/java/com/example/booktobook/SearchFragment.java
@@ -1,19 +1,103 @@
 package com.example.booktobook;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class SearchFragment extends Fragment {
 
+    AutoCompleteTextView autoCompleteTextView;
+    private ArrayList<String> data;
+    Button search;
+
+    private SearchActivity activity;
+    private OnDataPassListener onDataPassListener;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search,container,false);
+
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        autoCompleteTextView = view.findViewById(R.id.autoCompleteTextView);
+        data = new ArrayList<>();
+        add();
+        autoCompleteTextView.setAdapter(new ArrayAdapter<String>(getContext(), android.R.layout.simple_dropdown_item_1line, data));
+        search = view.findViewById(R.id.search);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(), SearchActivity.class);
+                String abc = autoCompleteTextView.getText().toString();
+                i.putExtra("abc",abc);
+
+                Log.d("tag", abc);
+
+                onDataPassListener.onDataPass(abc);
+                // activity.onDataPass(abc);
+                //onDataPassListener.onDataPass(abc);
+
+                startActivity(i);
+
+            }
+        });
+        return view;
+
+
     }
+
+    private void add() {
+        //예시 데이터
+        data.add("자바의 정석");
+        data.add("자바의 정석2");
+        data.add("코스모스");
+        data.add("열두 발자국");
+        data.add("사피엔스");
+        data.add("Animal");
+
+    }
+
+    public interface OnDataPassListener {
+        String onDataPass(String data);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            SearchActivity activity= new SearchActivity();
+            onDataPassListener=(OnDataPassListener) activity;
+        } catch (ClassCastException e) {
+//            throw new ClassCastException(context.toString()
+//                    + " must implement Listener");
+        }
+
+    }
+
+    @Override
+    public void onDetach () {
+        onDataPassListener = null;
+        super.onDetach();
+
+    }
+
+
 }
+
+
